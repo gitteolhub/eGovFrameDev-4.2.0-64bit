f0e816cc89027bf0255ab421368827472eeccff4
Admin <Admin@DESKTOP-52351T8>
2024-08-05 13:08:42 +0900

    first commit

---
+++ demoGradle/src/main/java/com/javateam/demoGradle3/controller/DemoController.java
@@ -0,0 +1,17 @@
+package com.javateam.demoGradle3.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+	@GetMapping("/")
+	public String demo(Model model) {
+		
+		System.out.println("demo");
+		model.addAttribute("java", 21);
+		return "demo";
+	}
+}
---
+++ demoGradle/src/test/java/com/javateam/demoGradle3/DemoGradle3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javateam.demoGradle3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGradle3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
