3253a27cdc3849d48e7734cee9427370a9834145
Admin <Admin@DESKTOP-52351T8>
2024-08-05 13:09:41 +0900

    first commit

---
+++ springboot-developer/src/test/java/com/javateam/demoGradle3/SpringbootDeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javateam.demoGradle3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDeveloperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
